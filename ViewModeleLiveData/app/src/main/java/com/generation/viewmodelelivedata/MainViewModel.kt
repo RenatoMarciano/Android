72ab44497264499d909bfaed7581d5a1f6d06b76
Renato Marciano <renato_vtr@hotmail.com>
2022-06-09 15:04:44 -0300

    ViewModel e LiveData

--- ViewModeleLiveData/app/src/main/java/com/generation/viewmodelelivedata/MainViewModel.kt
+++ ViewModeleLiveData/app/src/main/java/com/generation/viewmodelelivedata/MainViewModel.kt
@@ -1,4 +1,14 @@
 package com.generation.viewmodelelivedata
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    var cont = MutableLiveData<Int>(0)
+
+    fun addNum() {
+        cont.value = cont.value?.plus(1)
+    }
+
 }
